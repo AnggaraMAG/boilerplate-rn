38f7466172513b6fc2008c2fcccfdf9b442776ac
Anggara1234 <anggaramag@gmail.com>
2020-04-15 16:01:04 +0700

    setup

---
+++ src/Feed.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Text, View, Button, TouchableOpacity} from 'react-native';
+import {styles} from './styles/styles';
+
+export class Feed extends Component {
+  render() {
+    return (
+      <View style={styles.center}>
+        <Text style={styles.title}> textInComponent </Text>
+        <TouchableOpacity>
+          <Button onPress={() => this.props.navigation.navigate('')} />
+          <Text style={styles.androidButtonText}>Go to FeedItem</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default Feed;
---
+++ src/styles/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const style = StyleSheet.create({
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 36,
+    marginBottom: 17,
+  },
+  androidButtonText: {
+    color: 'blue',
+    fontSize: 20,
+  },
+});
